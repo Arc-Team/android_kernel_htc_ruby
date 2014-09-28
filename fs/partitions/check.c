86b32a6e6d3d9a4c148838e7ec950337d39baeec
JoseGalRe <josegalre@gmail.com>
2014-09-28 17:05:53 -0600

    Support volname for HTC devices with proc/emmc support
    Gets data from emmc_partitions and write it down on mmcblk0pX partitions
    this enables vold by-name mappings

--- fs/partitions/check.c
+++ fs/partitions/check.c
@@ -21,6 +21,7 @@
 #include <linux/ctype.h>
 #include <linux/genhd.h>
 #include <linux/blktrace_api.h>
+#include <mach/board_htc.h>
 
 #include "check.h"
 
@@ -479,6 +480,12 @@ struct hd_struct *add_partition(struct gendisk *disk, int partno,
 		dev_set_name(pdev, "%sp%d", dname, partno);
 	else
 		dev_set_name(pdev, "%s%d", dname, partno);
+
+	if (!strncmp(dev_name(pdev), "mmcblk0p", 8)) {
+		const char *pname = get_partition_name_by_num(p->partno);
+		if (pname)
+			snprintf(p->info->volname, PARTITION_META_INFO_VOLNAMELTH, pname);
+	}
 
 	device_initialize(pdev);
 	pdev->class = &block_class;
